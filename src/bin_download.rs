c321eb669fd59863040c750a7715533707b06970
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-16 13:48:07 -0700

    mention CC license terms of data files

--- src/bin_download.rs
+++ src/bin_download.rs
@@ -1,2 +1,9 @@
+/* Dictionaries are compiled from Wiktionary which gives them all
+   a CreativeCommons Attribution Share-Alike License (https://creativecommons.org/licenses/by-sa/3.0/).
+   The same license is applied to any additions or modifications
+   made by maintainers and contributors of the rhyme_dictionary
+   project.
+*/
+
 fn main() {
 }
